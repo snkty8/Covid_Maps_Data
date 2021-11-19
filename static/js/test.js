20e8078da0cadc253079f5685f53e0e61775305e
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-18 22:41:28 -0500

    test 5

--- static/js/test.js
+++ static/js/test.js
@@ -1,3 +1,7 @@
 var json = require('/Users/snkty/OneDrive/Desktop/Respositories/Covid_Maps_Data/Resources/sum_counts_per_months.json'); //(with path)
-//console.log(json[277]['Country/Region'])
-console.log(json.length)
+console.log(json[277]['Country/Region'])
+
+
+var countryRegion = json.forEach(function(obj) { console.log(obj['Country/Region']); });
+var Latitude = json.forEach(function(obj) { console.log(obj['Latitude']); });
+console.log(countryRegion)
