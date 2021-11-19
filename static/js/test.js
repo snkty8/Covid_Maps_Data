f943a76cca7c0c4eca5ac08f8da60bccf2d0531a
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-18 23:13:59 -0500

    test 7

--- static/js/test.js
+++ static/js/test.js
@@ -4,4 +4,30 @@ console.log(json[277]['Country/Region'])
 
 var countryRegion = json.forEach(function(obj) { console.log(obj['Country/Region']); });
 var Latitude = json.forEach(function(obj) { console.log(obj['Latitude']); });
-console.log(countryRegion)
+var Longitude = json.forEach(function(obj) { console.log(obj['Longitude']); });
+var Jan_2020 = json.forEach(function(obj) { console.log(obj['Jan_2020']); });
+var Jan_2020 = json.forEach(function(obj) { console.log(obj['Jan_2020']); });
+var Feb_2020 = json.forEach(function(obj) { console.log(obj['Feb_2020']); });
+var March_2020 = json.forEach(function(obj) { console.log(obj['March_2020']); });
+var April_2020 = json.forEach(function(obj) { console.log(obj['April_2020']); });
+var May_2020 = json.forEach(function(obj) { console.log(obj['May_2020']); });
+var June_2020 = json.forEach(function(obj) { console.log(obj['June_2020']); });
+var July_2020 = json.forEach(function(obj) { console.log(obj['July_2020']); });
+var Aug_2020 = json.forEach(function(obj) { console.log(obj['Aug_2020']); });
+var Sep_2020 = json.forEach(function(obj) { console.log(obj['Sep_2020']); });
+var Oct_2020 = json.forEach(function(obj) { console.log(obj['Oct_2020']); });
+var Nov_2020 = json.forEach(function(obj) { console.log(obj['Nov_2020']); });
+var Dec_2020 = json.forEach(function(obj) { console.log(obj['Dec_2020']); });
+
+var Jan_2021 = json.forEach(function(obj) { console.log(obj['Jan_2021']); });
+var Jan_2021 = json.forEach(function(obj) { console.log(obj['Jan_2021']); });
+var Feb_2021 = json.forEach(function(obj) { console.log(obj['Feb_2021']); });
+var March_2021 = json.forEach(function(obj) { console.log(obj['March_2021']); });
+var April_2021 = json.forEach(function(obj) { console.log(obj['April_2021']); });
+var May_2021 = json.forEach(function(obj) { console.log(obj['May_2021']); });
+var June_2021 = json.forEach(function(obj) { console.log(obj['June_2021']); });
+var July_2021 = json.forEach(function(obj) { console.log(obj['July_2021']); });
+var Aug_2021 = json.forEach(function(obj) { console.log(obj['Aug_2021']); });
+var Sep_2021 = json.forEach(function(obj) { console.log(obj['Sep_2021']); });
+var Oct_2021 = json.forEach(function(obj) { console.log(obj['Oct_2021']); });
+
