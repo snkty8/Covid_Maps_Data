5469ff89bd8b5a50e0e52a414719c1107091f52f
snkty8 <78936833+snkty8@users.noreply.github.com>
2022-01-25 23:46:16 -0500

    variables 2

--- static/js/test_variables.js
+++ static/js/test_variables.js
@@ -1,5 +1,29 @@
-let latitude = data[0].map(row => row.Latitude);
-console.log(latitude)
+let latitude = data[0].map(row => row.Latitude)
 
-let countryRegion = data[0].map(row => row["Country/Region"]);
-console.log(countryRegion)
+let longitude = data[0].map(row => row.Longitude)
+
+let countryRegion = data[0].map(row => row["Country/Region"])
+
+let jan2020 = data[0].map(row => row.Jan_2020)
+
+let feb2020 = data[0].map(row => row.Feb_2020)
+
+let march2020 = data[0].map(row => row.March_2020)
+
+let april2020 = data[0].map(row => row.April_2020)
+
+let may2020 = data[0].map(row => row.May_2020)
+
+let june2020 = data[0].map(row => row.June_2020)
+
+let july2020 = data[0].map(row => row.July_2020)
+
+let aug2020 = data[0].map(row => row.Aug_2020)
+
+let sep2020 = data[0].map(row => row.Sep_2020)
+
+let oct2020 = data[0].map(row => row.Oct_2020)
+
+let nov2020 = data[0].map(row => row.Nov_2020)
+
+let dec2020 = data[0].map(row => row.Dec_2020)
