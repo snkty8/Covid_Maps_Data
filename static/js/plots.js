7eeb07b4635d8cb2fd85c26a5b7abd75fafcc43b
snkty8 <78936833+snkty8@users.noreply.github.com>
2022-02-05 21:08:44 -0500

    plots 5

--- static/js/plots.js
+++ static/js/plots.js
@@ -15,8 +15,8 @@
 
 let data1 = [{
   // Afghanistan
-  x: ['Jan2020', 'Feb2020', 'March2020', 'April2020', 'May2020', 'June2020', 'July2020', 'August2020', 'Sept2020', 'Oct2020', 'Nov2020', 'Dec2020'],
-  y: [jan2020[0], feb2020[0], march2020[0], april2020[0], may2020[0], june2020[0], july2020[0], aug2020[0], sep2020[0], oct2020[0], nov2020[0], dec2020[0]],
+  x: ['Jan2020', 'Feb2020', 'March2020', 'April2020', 'May2020', 'June2020', 'July2020', 'August2020', 'Sept2020', 'Oct2020', 'Nov2020', 'Dec2020', 'Jan2021', 'Feb2021', 'March2021','April2021', 'May2021','June2021', 'July2021', 'Aug2021', 'Sept2021', 'Oct2021'],
+  y: [jan2020[0], feb2020[0], march2020[0], april2020[0], may2020[0], june2020[0], july2020[0], aug2020[0], sep2020[0], oct2020[0], nov2020[0], dec2020[0],jan2021[0], feb2021[0], march2021[0], april2021[0], may2021[0], june2021[0], july2021[0], aug2021[0], sep2021[0], oct2021[0]],
   type: "bar"
 }];
 
